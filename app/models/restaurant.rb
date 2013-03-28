4df49b3f6b144e9e7bf1d0cabedda1eea6195489
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-28 12:26:56 -0400

    Add has_many reservations relationship to User and Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :category
+  has_many :reservations
   attr_accessible :address, :description, :name, :phone, :picture, :category, :capacity
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :reservations
+
   attr_accessible :name, :email, :password, :password_confirmation
   validates :name, :email, :presence => true
-  validates :email, :uniqueness => true
-  
+  validates :email, :uniqueness => true  
   # We will need to limit this to :on => [:create] once we have a 
   # edit user page.
   validates :password_confirmation, :presence => true 
+
+
 end
