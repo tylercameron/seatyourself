c88c595585f70a91731a895ef39cb92555efb9cb
Michael Fagan <mfagan@gmail.com>
2013-09-13 10:59:37 -0400

    adding datetimes to reservations. sorta necessary. also autosetting the end time to two hours after the start time

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,13 @@ class Reservation < ActiveRecord::Base
   # attributes
   attr_accessible :begin_time, :end_time, :notes
 
+  after_initialize :set_end_time
+  def set_end_time
+    if self.begin_time
+      self.end_time ||= self.begin_time + 2.hours
+    end
+  end
+
   # assocations
   belongs_to :restaurant
   belongs_to :user
