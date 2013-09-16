737c393bd15f0d5ea513e06042ba49c526f64382
Michael Fagan <mfagan@gmail.com>
2013-09-16 17:24:56 -0400

    slightly improved factory for reservation

--- test/factories/reservations.rb
+++ test/factories/reservations.rb
@@ -1,10 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :reservation do
-    begin_time "1980-01-01 17:00:00"
+    begin_time "1980-01-01 17:00:00" # switch to something more dynamic? e.g. some date tomorrow
     restaurant { FactoryGirl.create(:restaurant) }
     user { FactoryGirl.create(:user) }
-    notes "Smoking area"
   end
 end
