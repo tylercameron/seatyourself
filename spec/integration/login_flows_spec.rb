c462d1a2fed8a18598adcffbc1ea9b5e9e971e5c
Michael Fagan <mfagan@gmail.com>
2013-09-17 13:29:11 -0400

    moving integration test from test::unit to rspec

--- spec/integration/login_flows_spec.rb
+++ spec/integration/login_flows_spec.rb
@@ -1,21 +1,20 @@
-require 'test_helper'
+require 'spec_helper'
 
-class LoginFlowsTest < ActionDispatch::IntegrationTest
-  test "user is redirected to home after signing in" do
+describe "login flows" do
+
+  it "redirects to home after signing in" do
     FactoryGirl.create(:user)
-
     get '/sessions/new'
     assert_response :success
 
     post_via_redirect '/sessions',
       :email => "dinerone@example.org",
       :password => "abc123"
-
     assert_equal '/', path
     assert_equal 'Welcome Diner One!', flash[:notice]
-  end  
+  end
 
-  test "user is signed in after signing up" do
+  it "signs in user after signing up" do
     get '/users/new'
     assert_response :success
 
@@ -25,14 +24,10 @@ class LoginFlowsTest < ActionDispatch::IntegrationTest
       :password => "def456",
       :password_confirmation => "def456" 
     }
-
     post_via_redirect '/users', { :user => user }
-      
-
     assert_equal '/', path
     assert_select '#login a', "Sign out"
     assert_equal 'Welcome Diner Two!', flash[:notice]
-
-  end  
+  end 
 
 end
