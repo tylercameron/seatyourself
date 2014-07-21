4a395d91b701096d5d03f23221678c2b76ee2c71
Julie Hache <elektrophile@gmail.com>
2014-07-20 21:41:50 -0400

    Add latitude and longitude data to Restaurant

---
+++ db/migrate/20140721012246_add_longitude_and_latitude_to_restaurant.rb
@@ -0,0 +1,8 @@
+class AddLongitudeAndLatitudeToRestaurant < ActiveRecord::Migration
+  def change
+    change_table :restaurants do |t|
+      t.decimal :longitude, precision: 9, scale: 6
+      t.decimal :latitude, precision: 9, scale: 6
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,49 +9,51 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130916212632) do
+ActiveRecord::Schema.define(version: 20140721012246) do
 
-  create_table "categories", :force => true do |t|
+  create_table "categories", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "reservations", :force => true do |t|
+  create_table "reservations", force: true do |t|
     t.datetime "begin_time"
     t.integer  "restaurant_id"
     t.integer  "user_id"
     t.text     "notes"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.integer  "people",        :default => 1
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "people",        default: 1
   end
 
-  add_index "reservations", ["restaurant_id"], :name => "index_reservations_on_restaurant_id"
-  add_index "reservations", ["user_id"], :name => "index_reservations_on_user_id"
+  add_index "reservations", ["restaurant_id"], name: "index_reservations_on_restaurant_id"
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
 
-  create_table "restaurants", :force => true do |t|
+  create_table "restaurants", force: true do |t|
     t.string   "name"
     t.string   "address"
     t.string   "phone"
     t.string   "picture"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.integer  "capacity",    :default => 100
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.integer  "capacity",                            default: 100
+    t.decimal  "longitude",   precision: 9, scale: 6
+    t.decimal  "latitude",    precision: 9, scale: 6
   end
 
-  add_index "restaurants", ["category_id"], :name => "index_restaurants_on_category_id"
+  add_index "restaurants", ["category_id"], name: "index_restaurants_on_category_id"
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
