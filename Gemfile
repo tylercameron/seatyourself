e0ca5715e8ce6209c0fd1c368c085105f0ac3c29
Julie Hache <elektrophile@gmail.com>
2015-03-09 07:48:23 -0400

    Update Rails to 4.2

--- Gemfile
+++ Gemfile
@@ -1,18 +1,25 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.1.5'
+gem 'rails', '4.2.0'
 gem 'sqlite3'
-gem 'jquery-rails'
-gem 'bcrypt-ruby'
-
-gem 'sass-rails'
-gem 'coffee-rails'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
 gem 'haml-rails'
-gem 'uglifier'
 
-group :test, :development do
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+gem 'bcrypt', '~> 3.1.7'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
   gem 'factory_girl_rails'
   gem 'rspec-rails'
   gem 'assert_difference' # using within rspec
-  gem 'pry'
+  gem 'pry-rails'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,49 +9,57 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130916212632) do
+ActiveRecord::Schema.define(version: 20141209163101) do
 
-  create_table "categories", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "authentications", force: :cascade do |t|
+    t.integer  "user_id",                null: false
+    t.string   "provider",   limit: 255, null: false
+    t.string   "uid",        limit: 255, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "reservations", :force => true do |t|
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
     t.datetime "begin_time"
     t.integer  "restaurant_id"
     t.integer  "user_id"
     t.text     "notes"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.integer  "people",        :default => 1
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "people",        default: 1
   end
 
-  add_index "reservations", ["restaurant_id"], :name => "index_reservations_on_restaurant_id"
-  add_index "reservations", ["user_id"], :name => "index_reservations_on_user_id"
+  add_index "reservations", ["restaurant_id"], name: "index_reservations_on_restaurant_id"
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
 
-  create_table "restaurants", :force => true do |t|
+  create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.string   "phone"
     t.string   "picture"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.integer  "capacity",    :default => 100
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "capacity",    default: 100
   end
 
-  add_index "restaurants", ["category_id"], :name => "index_restaurants_on_category_id"
+  add_index "restaurants", ["category_id"], name: "index_restaurants_on_category_id"
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
