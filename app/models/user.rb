78683142ef22ca040d3ca4b8174282e6b8557731
Jessica <jhai_alex@hotmail.com>
2016-11-11 15:03:57 -0500

    Updated User Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+	has_secure_password
+	validates :name, :email, :phone, :password, :password_confirmation, presence: true
+
+
 	has_many :reservations
 	has_many :restaurants, through: :reservations
 end
