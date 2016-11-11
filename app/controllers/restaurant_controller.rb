afd7cca274940c08414c15744b4689fda8db1045
prisara <prisaramorale@gmail.com>
2016-11-11 14:59:49 -0500

    Add controller methods in restaurant controller

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,16 +1,44 @@
 class RestaurantController < ApplicationController
-	def new
-	end
+  def index
+    @restaurants = Restaurants.all
+  end
 
-	def show
-	end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-	def create
-	end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-	def destroy
-	end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_url
+    else
+      render :new
+    end
+  end
 
-	def update
-	end
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url
+  end
+
+  def update
+    @restaurants = restaurant.find(restaurant[:id])
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_url(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :cuisine, :price, :street_address, :neighbourhood, :phone)
+  end
+
 end
