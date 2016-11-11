923118f57b6fe1045372ef4e493f6e764d07e415
prisara <prisaramorale@gmail.com>
2016-11-11 15:27:44 -0500

    Add validations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
-	belongs_to :user
-	belongs_to :restaurant 
+  validates :date, :time, :number_of_seats, presence: true
+  validates :number_of_seats, numericality: {only_integer: true}
+
+  belongs_to :user
+  belongs_to :restaurant
 end
