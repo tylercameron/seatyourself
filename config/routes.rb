cb4ff8f517092ffc779e882126330cde097a5ba4
Tyler <tcameronsmith@gmail.com>
2016-11-12 12:56:24 -0500

    change user routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 	resources :reservations
 	resources :restaurants
-	resource :users
+	resources :users
 	resources :sessions, only: [:new, :create, :destroy]
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
