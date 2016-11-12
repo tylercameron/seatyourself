0c63ed195b7818cae14149ada36081461a66866f
Tyler <tcameronsmith@gmail.com>
2016-11-12 11:12:13 -0500

    bcrypt gem added, new user view page now replaced create page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+	root 'users#new'
+
 	resources :reservations
 	resources :restaurants
-	resource :users
+	resource :users, only: %i(new create)
 	resources :sessions, only: [:new, :create, :destroy]
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
