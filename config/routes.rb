dc50e1af12db7637f37de7b071e80583150a1f0e
Tyler <tcameronsmith@gmail.com>
2016-11-12 10:36:02 -0500

    routes changed for users, removed index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 	resources :reservations
 	resources :restaurants
-	resources :users
+	resource :users
 	resources :sessions, only: [:new, :create, :destroy]
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
