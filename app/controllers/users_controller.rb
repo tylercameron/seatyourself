7b944e9e69ec0ac22c4aa5214c9624fc536549e2
Tyler <tcameronsmith@gmail.com>
2016-11-12 12:45:34 -0500

    show, edit, update methods made in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update_attributes(user_params)
+			redirect_to user_url(@user)
+		else
+			render :edit
+		end
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:email, :password, :password_confirmation, :name, :phone)
