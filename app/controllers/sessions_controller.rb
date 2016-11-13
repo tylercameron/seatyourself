dbdba2c3d3c107e5959ca1c37bc853492e3007b4
prisara <prisaramorale@gmail.com>
2016-11-13 16:35:15 -0500

    Make changes to headings and text

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session [:user_id] == user.id
+      session[:user_id] == user.id
 
       redirect_to reservations_url, notice: "Logged in!"
     else
