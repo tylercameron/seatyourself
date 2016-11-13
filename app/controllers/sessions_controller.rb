7e34f9cf875dc5b73c78d3a89c56a2ac3311afbc
terious78 <taesuk.moon@gmail.com>
2016-11-13 16:19:51 -0500

    edit session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
-
-      session [:user_id] = user.id
+      session[:user_id] == user.id
       redirect_to reservation_url, notice: "Logged in!"
     else
       flash.now[:alert] = "Invalid email or password"
