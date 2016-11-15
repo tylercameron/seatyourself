b060ac3864bcc793ae4e53d7a45cc95aeac59460
prisara <prisaramorale@gmail.com>
2016-11-15 11:36:15 -0500

    Made lots of styling changes; please ignore changes made to controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -20,16 +20,29 @@ class ReservationsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
-  def create
-    @reservation = Reservation.new(reservation_params)
-    @reservation.user = current_user
-      if @reservation.save
-        redirect_to reservations_url
-      else
-        render :new
-      end
-  end
+  # def create
+  #   @reservation = Reservation.new(reservation_params)
+  #   @reservation.user = current_user
+  #     if @reservation.save
+  #       redirect_to restaurant_reservations_url
+  #     else
+  #       render :new
+  #     end
+  # end
 
+  def create
+     @reservation = Reservation.new(reservation_params)
+     @restaurant = Restaurant.find(reservation_params[:restaurant_id])
+     if @restaurant.available(reservation_params[:number_of_seats].to_i,reservation_params[:time_of_reservation].to_time)
+       if @reservation.save
+         redirect_to reservations_url
+       else
+         flash.now[:notice] = "Sorry, you cant sit with us at that time! Please try again, we want you at our table :)"
+         render :new
+       end
+     end
+   end
+   
   def edit
     @reservation = Reservation.find(params[:id])
     @restaurant = Restaurant.find(params[:restaurant_id])
